d53acdf54736bc6727d48a794c88189fce0dc5d1
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-07-28 15:33:28 +0700

    feat: add some more components from shadcn/ui

---
+++ src/shared/components/ui/stateful-button.tsx
@@ -0,0 +1,19 @@
+import { LoaderCircleIcon } from 'lucide-react';
+
+import { Button } from '@/shared/components/ui/button';
+
+export type StatefulButtonProps = React.ComponentProps<typeof Button> & {
+  loading?: boolean;
+};
+
+export function StatefulButton({
+  loading,
+  children,
+  ...props
+}: StatefulButtonProps) {
+  return (
+    <Button disabled={loading} {...props}>
+      {loading && <LoaderCircleIcon className="animate-spin" />} {children}
+    </Button>
+  );
+}
