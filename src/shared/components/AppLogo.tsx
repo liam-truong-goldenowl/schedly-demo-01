d0dd43311f4321761510dd247d090e91221b8140
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-07-31 11:20:55 +0700

    feat: update app logo

--- src/shared/components/AppLogo.tsx
+++ src/shared/components/AppLogo.tsx
@@ -2,7 +2,10 @@ import Link from 'next/link';
 
 export function AppLogo() {
   return (
-    <Link href={'/'} className="text-heading-40">
+    <Link
+      href={'/'}
+      className="text-heading-40 from-primary to-primary/20 bg-gradient-to-br bg-clip-text font-bold text-transparent"
+    >
       Schedly
     </Link>
   );
