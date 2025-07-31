9579ea758d4d19abfa7d16f627108dcc8990c9e6
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-07-31 15:42:51 +0700

    refactor: name api for client and server separately

--- src/shared/lib/api.ts
+++ src/shared/lib/api.ts
@@ -1,13 +1,15 @@
+import { redirect } from 'next/navigation';
 import { getSession } from 'next-auth/react';
+import { getServerSession } from 'next-auth';
 import { createFetch } from '@better-fetch/fetch';
 
 import { env } from './env';
 
-export const api = createFetch({
+export const clientApi = createFetch({
   baseURL: env.NEXT_PUBLIC_API_URL,
 });
 
-export const apiWithAuth = createFetch({
+export const clientApiWithAuth = createFetch({
   baseURL: env.NEXT_PUBLIC_API_URL,
   credentials: 'include',
   onRequest: async (context) => {
@@ -16,6 +18,22 @@ export const apiWithAuth = createFetch({
     if (!session) {
       window.location.href = '/login';
       return context;
+    }
+
+    context.headers.set('Authorization', `Bearer ${session.accessToken}`);
+
+    return context;
+  },
+});
+
+export const serverApiWithAuth = createFetch({
+  baseURL: env.NEXT_PUBLIC_API_URL,
+  credentials: 'include',
+  onRequest: async (context) => {
+    const session = await getServerSession();
+
+    if (!session) {
+      redirect('/login');
     }
 
     context.headers.set('Authorization', `Bearer ${session.accessToken}`);
