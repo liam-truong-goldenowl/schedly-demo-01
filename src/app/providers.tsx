92595f759ef2068ea3455ce918d1a32084400097
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-08-01 08:56:23 +0700

    feat: setup tanstack query for mutation

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,7 @@ import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import { Toaster } from '@/shared/components/ui/sonner';
 import AuthProvider from '@/shared/components/providers/AuthProvider';
 import { ThemeProvider } from '@/shared/components/providers/ThemeProvider';
+import { QueryProvider } from '@/shared/components/providers/QueryProvider';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -15,7 +16,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
     >
       <NextIntlClientProvider>
         <AuthProvider>
-          <NuqsAdapter>{children}</NuqsAdapter>
+          <NuqsAdapter>
+            <QueryProvider>{children}</QueryProvider>
+          </NuqsAdapter>
         </AuthProvider>
       </NextIntlClientProvider>
       <Toaster position="top-center" closeButton richColors />
