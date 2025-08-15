5c4711dfc51ffd6ed3efac0a2abcdc9c5a9ca60a
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-08-15 09:41:32 +0700

    feat: prepare landing page for l&d demo

--- src/app/(guest)/page.tsx
+++ src/app/(guest)/page.tsx
@@ -1,3 +1,21 @@
+import Link from 'next/link';
+
+import { Heading } from '@/shared/components/layout/Heading';
+
 export default function GuestPage() {
-  return <div>Marketing page</div>;
+  return (
+    <main className="mx-auto max-w-prose py-4">
+      <header>
+        <Heading>Marketing Page</Heading>
+      </header>
+      <nav className="flex items-center gap-4">
+        <Link className="underline" href="/login">
+          Login
+        </Link>
+        <Link className="underline" href="/sign-up">
+          Sign up
+        </Link>
+      </nav>
+    </main>
+  );
 }
