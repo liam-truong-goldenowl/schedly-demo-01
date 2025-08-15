625e26dfe98f304a350a6310bdca84da32f65429
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-08-15 09:37:17 +0700

    feat: remove quick create button

--- src/shared/components/layout/NavMain.tsx
+++ src/shared/components/layout/NavMain.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { IconPlus, type Icon } from '@tabler/icons-react';
+import { type Icon } from '@tabler/icons-react';
 
 import {
   SidebarMenu,
@@ -28,17 +28,6 @@ export function NavMain({
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-5">
-        <SidebarMenu>
-          <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton
-              tooltip="Quick Create"
-              className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
-            >
-              <IconPlus />
-              <span>Create</span>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
