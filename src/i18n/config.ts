de5f4cf2b990a2e4cfd3f9720b1286b5d0048642
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-08-20 19:18:11 +0700

    feat: update auth pages

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,4 +1,4 @@
-export const LOCALES = ['en', 'de', 'vi'] as const;
+export const LOCALES = ['en', 'vi'] as const;
 export const DEFAULT_LOCALE: Locale = 'en';
 
 export type Locale = (typeof LOCALES)[number];
