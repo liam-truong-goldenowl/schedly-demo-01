d7b33584085ab2108a3cf5c9dd182f6ccd9b9865
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-08-25 11:42:47 +0700

    chore: remove unused files

--- src/modules/meetings/services/query.ts
+++
@@ -1,9 +0,0 @@
-import { clientApiWithAuth } from '@/shared/lib/client-api';
-
-export async function fetchMeetings(query: {
-  period: string;
-  startDate?: string;
-  endDate?: string;
-}) {
-  return clientApiWithAuth('@get/meetings', { params: query });
-}
