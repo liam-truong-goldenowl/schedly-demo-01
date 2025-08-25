e86e30a72e086b11911d8bc76cd593440d236f20
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-08-25 17:03:31 +0700

    refactor: use button instead of radio input for switch schedule

--- src/modules/availability/components/SwitchSchedule.tsx
+++ src/modules/availability/components/SwitchSchedule.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { cn } from '@/shared/lib/utils';
 import { MapItem } from '@/shared/components/MapItem';
 
 import { useActiveSchedule } from '../hooks/useActiveSchedule';
@@ -10,33 +11,23 @@ export function SwitchSchedule() {
   const { activeScheduleId, setActiveScheduleId } = useActiveSchedule();
 
   return (
-    <div>
-      <fieldset className="bg-border flex w-fit gap-(--divider-size) pe-(--divider-size) [--divider-size:1px]">
-        <legend className="sr-only">select schedule</legend>
-        <MapItem
-          items={schedules}
-          itemKey={({ item }) => item.id}
-          render={({ item, key }) => (
-            <div key={key} className="flex">
-              <input
-                type="radio"
-                id={key.toString()}
-                name="schedule"
-                value={item.id}
-                defaultChecked={item.id === activeScheduleId}
-                className="peer sr-only"
-                onChange={() => setActiveScheduleId(item.id)}
-              />
-              <label
-                htmlFor={key.toString()}
-                className="peer-checked:bg-accent peer-checked:border-b-border bg-background active:bg-accent text-copy-14 grow cursor-pointer border-x border-y-4 border-transparent px-6 py-4 hover:bg-gray-50"
-              >
-                {item.name} {item.isDefault && '(default)'}
-              </label>
-            </div>
-          )}
-        />
-      </fieldset>
+    <div className="bg-border flex w-fit flex-wrap gap-(--divider-size) pe-(--divider-size) [--divider-size:1px]">
+      <MapItem
+        items={schedules}
+        itemKey={({ item }) => item.id}
+        render={({ item, key }) => (
+          <button
+            key={key}
+            className={cn(
+              'bg-background active:bg-accent text-copy-14 grow cursor-pointer border-x border-y-4 border-transparent px-6 py-4 hover:bg-gray-50',
+              item.id == activeScheduleId && 'bg-accent border-b-border',
+            )}
+            onClick={() => setActiveScheduleId(item.id)}
+          >
+            {item.name} {item.isDefault && '(default)'}
+          </button>
+        )}
+      />
     </div>
   );
 }
