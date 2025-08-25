9a62a8b08eba6e41a44e0f63bd6967a213b1b9c2
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-08-25 12:20:22 +0700

    style: organize imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,5 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from 'eslint-plugin-storybook';
-
 import { FlatCompat } from '@eslint/eslintrc';
+import storybook from 'eslint-plugin-storybook';
 import perfectionist from 'eslint-plugin-perfectionist';
 
 const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
